a1ab2d8b691020c6b2600e554e391d4f3286e3a1
TusharC19 <tusharchaturvedi199unova@gmail.com>
2025-09-28 23:47:48 +0530

    added navbar

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,7 @@ import {Button} from "@/components/ui/button";
 const Page = () => {
   return (
     <div>
-        <h1 className="text-2xl underline">
-            Welcome to LearningCompanion
-        </h1>
-        <Button>Fuvkkk</Button>
+
     </div>
   )
 }
